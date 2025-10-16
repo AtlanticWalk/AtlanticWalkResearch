116555d1bd65135946628360c9e1671f96130acf
AtlanticWalk <g.rentrop@live.com>
2025-10-16 15:29:43 -0400

    Fix contact section in index.js

--- pages/index.js
+++ pages/index.js
@@ -201,7 +201,7 @@ export default function AtlanticWalkResearch() {
         <section className="max-w-md ml-auto mr-[8rem] text-right">
           <h2 className="text-2xl font-semibold mb-4"></h2>
           <p className="text-lg text-black font-semibold mb-3">
-            Contact:{" "}
+            {" "}
             <a
               href="mailto:grentrop@atlanticwalkresearch.com"
               className="text-black hover:underline"
