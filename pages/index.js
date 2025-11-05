11960d59df6b598864423eed46a7843a353c6e3d
AtlanticWalk <g.rentrop@live.com>
2025-11-04 23:24:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -522,8 +522,8 @@ export default function AtlanticWalkResearch({ reports = [] }) {
     bg-scroll md:bg-fixed
     ${
       page === "home"
-        ? "bg-[url('/backgrounds/home-bg-mobile.jpg')] md:bg-[url('/backgrounds/home-bg.jpg')]"
-        : "bg-[url('/backgrounds/other-bg-mobile.jpg')] md:bg-[url('/backgrounds/other-bg.jpg')]"
+        ? "bg-[url('/backgrounds/home-bg.jpg')] md:bg-[url('/backgrounds/home-bg.jpg')]"
+        : "bg-[url('/backgrounds/mobile-bg.jpg')] md:bg-[url('/backgrounds/other-bg.jpg')]"
     }
   `}
 >
