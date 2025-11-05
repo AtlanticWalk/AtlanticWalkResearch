18a6533a807db078239b588e3016ac036284d1cf
AtlanticWalk <g.rentrop@live.com>
2025-11-05 10:24:05 -0500

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Ensures Tailwind’s mobile breakpoints work */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
