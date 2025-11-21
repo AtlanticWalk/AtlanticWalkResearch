cb65ce35806656f10191bdd4ff7997de4db59fa5
AtlanticWalk <g.rentrop@live.com>
2025-11-21 16:02:30 -0500

    Add new report entry for Avadel Update

--- data/reportsMeta.js
+++ data/reportsMeta.js
@@ -5,8 +5,14 @@ export const reportsMeta = [
     ticker: "AVDL",
     date: "2025-10-25",
   },
- 
+ {
+  slug: "Avadel-Update",
+  title: "Avadel Update: Four Bites In, Jazz Is Now The Natural Final Buyer",
+  ticker: "NASDAQ: AVDL",
+  date: "2025-11-21" 
+},
   // add more here as needed
 
 ];
+
 
