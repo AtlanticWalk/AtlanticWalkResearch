37d6ee353c4abcbbdaa573c1baf0dfe6c6623497
AtlanticWalk <g.rentrop@live.com>
2025-11-21 17:53:29 -0500

    Change section to have rounded corners
    Updated the 'about' section to have rounded corners.

--- pages/index.js
+++ pages/index.js
@@ -283,7 +283,7 @@ export default function AtlanticWalkResearch({ reports = [] }) {
     // ---------------- ABOUT ----------------
     if (page === "about") {
       return (
-        <section className="bg-neutral-600 bg-opacity-20 max-w-2xl ml-auto mr-[4rem] text-white text-left pr-8 pb-20 space-y-6">
+        <section className="bg-neutral-600 bg-opacity-20 rounded-xl max-w-2xl ml-auto mr-[4rem] text-white text-left pr-8 pb-20 space-y-6">
           <p>
             <strong>Mission:</strong> Turn complex policy, capital allocation,
             and structural change into clear, asymmetric investment ideas
