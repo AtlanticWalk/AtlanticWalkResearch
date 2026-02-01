cca846cbad1e6a17ce8cfd4aae428d87ba51892f
AtlanticWalk <g.rentrop@live.com>
2026-01-31 19:15:25 -0500

    Integrate Google Analytics into the application
    Added Google Analytics integration to the app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 // pages/_app.js
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+      {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
+    </>
   );
 }
