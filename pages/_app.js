d420159bbccd5584f040913719bf650bbe5b2d84
AtlanticWalk <g.rentrop@live.com>
2026-01-31 19:16:28 -0500

    Replace dynamic GA ID with static value

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,13 @@ import Layout from "../components/Layout";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }) {
-  const gaId = process.env.NEXT_PUBLIC_GA_ID;
-
   return (
     <>
       <Layout>
         <Component {...pageProps} />
       </Layout>
 
-      {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
+      <GoogleAnalytics gaId="G-7EYNZ1NEKX" />
     </>
   );
 }
