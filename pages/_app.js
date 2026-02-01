ecec12ca237ac54cf5d411e6c18e203e87295ae8
AtlanticWalk <g.rentrop@live.com>
2026-01-31 19:21:16 -0500

    Integrate Google Analytics with useEffect hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,46 @@
 // pages/_app.js
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const GA_ID = "G-7EYNZ1NEKX";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === "function") {
+        window.gtag("config", GA_ID, { page_path: url });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return (
     <>
+      {/* Load GA4 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+        `}
+      </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
-
-      <GoogleAnalytics gaId="G-7EYNZ1NEKX" />
     </>
   );
 }
